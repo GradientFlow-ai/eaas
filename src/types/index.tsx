e0e98d010dddc813d6f9cb8a775dfd3949edda77
coyotespike <timothy.roy@protonmail.ch>
2023-05-11 18:35:35 -0500

    Modify type and first upload function

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,1 +1,11 @@
 export type Icon = React.ComponentType<React.ComponentProps<"svg">>;
+export interface EmbeddingsFileInfo {
+  userEmail: string;
+  fileName?: string;
+  uuid?: string;
+  description?: string;
+  embeddingsModel?: string;
+  embeddingsName?: string;
+  contributorName?: string;
+  license?: string;
+}
