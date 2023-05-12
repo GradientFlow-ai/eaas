a0145dd203100c1bc5f5034679c307e2a6684a9c
coyotespike <timothy.roy@protonmail.ch>
2023-05-12 08:47:00 -0500

    Fix type errors in context hook

--- src/state/signInModalContext.ts
+++ src/state/signInModalContext.ts
@@ -1,11 +1,15 @@
 import { createContext, useContext, Dispatch, SetStateAction } from "react";
 
 interface SignInModalContextProps {
-  setShowSignInModal?: Dispatch<SetStateAction<boolean>>;
+  setShowSignInModal: Dispatch<SetStateAction<boolean>>;
 }
 
 export const SignInModalContext = createContext<SignInModalContextProps | null>(null);
 
 export function useSignInModalContext() {
-  return useContext(SignInModalContext);
+  const context = useContext(SignInModalContext);
+  if (!context) {
+    throw new Error("useSignInModalContext must be used within a SignInModalContextProvider");
+  }
+  return context;
 }
